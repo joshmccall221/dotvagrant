5468a861bc57c7e9170b1fd55622b45d7259b9ba
Josh McCall <jmccall@sourceallies.com>
2015-06-02 10:50:01 -0500

    update

--- Vagrantfile
+++ Vagrantfile
@@ -65,7 +65,10 @@ Vagrant.configure(2) do |config|
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
   # config.vm.provision "shell", inline: <<-SHELL
-  #config.vm.provision "shell", path: "vagrantrunme.sh"
+  #.
+  #.
+  config.vm.provision "shell", path: "vagrantrunme.sh"
+  #
   #   sudo apt-get update
   #   sudo apt-get install -y apache2
   # SHELL
--- vagrantrunme.sh
+++ vagrantrunme.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 #Dependencies
+echo 'cd /home/vagrant'
 cd /home/vagrant
+echo 'sudo apt-get update'
 sudo apt-get update
-#sudo -u root@localhost
 
 
 echo whoami
-echo whoami
-echo whoami
-echo whoami
-echo whoami
-whoami
-whoami
-whoami
-whoami
-whoami
 whoami
 
 echo 'sudo apt-get -y install git curl vim vim-nox'
 sudo apt-get -y install git curl vim vim-nox
 
 
-echo '#Solarized for vim and terminal'
-echo '#Solarized for vim and terminal'
-echo '#Solarized for vim and terminal'
-echo '#Solarized for vim and terminal'
+echo '######################################'
+echo '#   Solarized for vim and terminal   #'
+echo '######################################'
 
 if [ ! -d "./gnome-terminal-colors-solarized"  ]; then
     echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
-    echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
-    echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
-    echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
-    echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
     su -c "git clone https://github.com/Anthony25/gnome-terminal-colors-solarized" vagrant
 fi
+echo '######################################'
+echo '#   Solarized for terminal           #'
+echo '######################################'
 
 echo 'cd ./gnome-terminal-colors-solarized'
 cd ./gnome-terminal-colors-solarized
@@ -46,6 +36,9 @@ su -c "./install.sh -s dark -p Default" vagrant
 echo 'cd /home/vagrant'
 cd /home/vagrant
 
+echo '######################################'
+echo '#   Solarized for vim                #'
+echo '######################################'
 if [ ! -f spf13.vim.sh ]; then
    su -c " curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh" vagrant
 fi
