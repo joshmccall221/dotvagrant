791e635e3d79d0800498f8b17bb7d70146e393c1
joshmccall221 <mail@prabir.me>
2015-06-03 10:16:18 -0500

    formatting

--- vagrantrunme.sh
+++ vagrantrunme.sh
@@ -2,92 +2,92 @@
 #Dependencies
   echo 'sudo apt-get update'
    sudo apt-get update > /dev/null 2>&1
-#   apt-get -y install ubuntu-desktop
-# 
-#   echo 'cd /home/vagrant'
-#   cd /home/vagrant
-# 
-#  
-#  echo whoami
-#  whoami
-#  
-#    echo 'sudo apt-get -y install git curl vim vim-nox tmux'
-#    sudo apt-get -y install git curl vim vim-nox tmux
-#    
-#    
-#     scho '######################################'
-#     echo '#   Solarized for vim and terminal   #'
-#     echo '######################################'
-#     
-#     if [ ! -d "./gnome-terminal-colors-solarized"  ]; then
-#         echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
-#         su -c "git clone https://github.com/Anthony25/gnome-terminal-colors-solarized" vagrant
-#     fi
-#     echo '######################################'
-#     echo '#   Solarized for terminal           #'
-#     echo '######################################'
-#     
-#     echo 'cd ./gnome-terminal-colors-solarized'
-#     cd ./gnome-terminal-colors-solarized
-#     echo 'sudo chmod +755 *.sh'
-#     sudo chmod +755 *.sh
-#     
-#     echo ' ./install.sh -s dark -p Default'
-#     su -c "./install.sh -s dark -p Default" vagrant
-#     
-#     echo 'cd /home/vagrant'
-#     cd /home/vagrant
-#     
-#     echo '######################################'
-#     echo '#   Solarized for vim                #'
-#     echo '######################################'
-#     if [ ! -f spf13.vim.sh ]; then
-#        su -c " curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh" vagrant
-#     fi
-#     echo 'echo color sol >> /home/vagrant/.vimrc'
-#     echo 'color sol' >> /home/vagrant/.vimrc
-#    
-#    
-#    echo '##################################################'
-#    echo '#   tmux: https://gist.github.com/diginc/8531848 #'
-#    echo '##################################################'
-#    
-#    if [ ! -f /home/vagrant/.bash_alliases  ]; then
-#         git clone https://gist.github.com/b329c4a84a274c906aa6.git bash_alliases
-#         cd bash_alliases
-#         cp .bash_aliases  /home/vagrant/.bash_aliases
-#         cd /home/vagrant
-#         source  /home/vagrant/.bash_alliases 
-#    fi
-#    
-#    if [ ! -f /home/vagrant/.tmux.conf ]; then
-#         git clone https://gist.github.com/8525360.git tmux_conf
-#         cd tmux_conf
-#         cp gistfile1.txt /home/vagrant/.tmux.conf
-#         cd /home/vagrant
-#        source /home/vagrant/.bashrc 
-#    fi
-#    
-#    echo 'tm' >> /home/vagrant/.bashrc 
-#    source /home/vagrant/.bashrc 
-#  
-#   sudo apt-get update > /dev/null 2>&1
-#   echo '#################################################################'
-#   echo '#  NPM: curl -sL https://deb.nodesource.com/setup | sudo bash - #'
-#   echo '#################################################################'
-#   
-#   cd /home/vagrant
-#   curl -sL https://deb.nodesource.com/setup | sudo bash -
-#   sudo apt-get install -y nodejs
-#  
-#  sudo apt-get update > /dev/null 2>&1
-#   echo '##################################################'
-#   echo '#  http-server: npm install http-server -g       #'
-#   echo '##################################################'
-#   cd /home/vagrant
-#   npm install http-server -g
-#  
-#  
+   apt-get -y install ubuntu-desktop
+ 
+   echo 'cd /home/vagrant'
+   cd /home/vagrant
+ 
+  
+  echo whoami
+  whoami
+  
+    echo 'sudo apt-get -y install git curl vim vim-nox tmux'
+    sudo apt-get -y install git curl vim vim-nox tmux
+    
+    
+     scho '######################################'
+     echo '#   Solarized for vim and terminal   #'
+     echo '######################################'
+     
+     if [ ! -d "./gnome-terminal-colors-solarized"  ]; then
+         echo 'sudo git clone https://github.com/Anthony25/gnome-terminal-colors-solarized'
+         su -c "git clone https://github.com/Anthony25/gnome-terminal-colors-solarized" vagrant
+     fi
+     echo '######################################'
+     echo '#   Solarized for terminal           #'
+     echo '######################################'
+     
+     echo 'cd ./gnome-terminal-colors-solarized'
+     cd ./gnome-terminal-colors-solarized
+     echo 'sudo chmod +755 *.sh'
+     sudo chmod +755 *.sh
+     
+     echo ' ./install.sh -s dark -p Default'
+     su -c "./install.sh -s dark -p Default" vagrant
+     
+     echo 'cd /home/vagrant'
+     cd /home/vagrant
+     
+     echo '######################################'
+     echo '#   Solarized for vim                #'
+     echo '######################################'
+     if [ ! -f spf13.vim.sh ]; then
+        su -c " curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh" vagrant
+     fi
+     echo 'echo color sol >> /home/vagrant/.vimrc'
+     echo 'color sol' >> /home/vagrant/.vimrc
+    
+    
+    echo '##################################################'
+    echo '#   tmux: https://gist.github.com/diginc/8531848 #'
+    echo '##################################################'
+    
+    if [ ! -f /home/vagrant/.bash_alliases  ]; then
+         git clone https://gist.github.com/b329c4a84a274c906aa6.git bash_alliases
+         cd bash_alliases
+         cp .bash_aliases  /home/vagrant/.bash_aliases
+         cd /home/vagrant
+         source  /home/vagrant/.bash_alliases 
+    fi
+    
+    if [ ! -f /home/vagrant/.tmux.conf ]; then
+         git clone https://gist.github.com/8525360.git tmux_conf
+         cd tmux_conf
+         cp gistfile1.txt /home/vagrant/.tmux.conf
+         cd /home/vagrant
+        source /home/vagrant/.bashrc 
+    fi
+    
+    echo 'tm' >> /home/vagrant/.bashrc 
+    source /home/vagrant/.bashrc 
+  
+   sudo apt-get update > /dev/null 2>&1
+   echo '#################################################################'
+   echo '#  NPM: curl -sL https://deb.nodesource.com/setup | sudo bash - #'
+   echo '#################################################################'
+   
+   cd /home/vagrant
+   curl -sL https://deb.nodesource.com/setup | sudo bash -
+   sudo apt-get install -y nodejs
+  
+  sudo apt-get update > /dev/null 2>&1
+   echo '##################################################'
+   echo '#  http-server: npm install http-server -g       #'
+   echo '##################################################'
+   cd /home/vagrant
+   npm install http-server -g
+  
+  
    sudo apt-get update > /dev/null 2>&1
     echo '########################################################'
     echo '#  Ruby: apt-get install rbenv                         #'
