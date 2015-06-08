7133c47a79aab76698cd2ae4fcc87dd13be1f34e
joshmccall221 <joshmccall221@gmail.com>
2015-06-07 21:58:08 -0500

    working zsh and chrome

--- vagrantrunme.sh
+++ vagrantrunme.sh
@@ -32,11 +32,11 @@ echo '##########################################'
 echo '##########################################'
 echo '# sudo apt-get -y install git curl vim vim-nox tmux gnome-terminal vim-gnome google-chrome'
 echo '##########################################'
-     sudo apt-get -y install git curl vim vim-nox tmux gnome-terminal vim-gnome  > /dev/null 2>&1
-     cd /tmp
-     wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb > /dev/null 2>&1
-     sudo dpkg -i google-chrome-stable_current_i386.deb > /dev/null 2>&1
-     sudo apt-get -f install > /dev/null 2>&1
+     sudo apt-get -y install git curl vim vim-nox tmux gnome-terminal vim-gnome
+#     cd /tmp
+#     wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb > /dev/null 2>&1
+#     sudo dpkg -i google-chrome-stable_current_i386.deb > /dev/null 2>&1
+#     sudo apt-get -f install > /dev/null 2>&1
 echo '######################################'
 echo '#   Solarized for vim and terminal   #'
 echo '######################################'
@@ -124,14 +124,40 @@ echo '########################################################'
      echo '#  Compass:  su -c "gem install comapass" vagrant      #'
      sudo apt-get update > /dev/null 2>&1
      sudo su -c "gem install compass > /dev/null 2>&1"  
-     echo '#  css_parser:  su -c "gem install css_parser" vagrant #'
-     sudo apt-get update > /dev/null 2>&1
-     sudo su -c "gem install css_parser > /dev/null 2>&1" 
-     sudo apt-get update > /dev/null 2>&1
+#     echo '#  css_parser:  su -c "gem install css_parser" vagrant #'
+#     sudo apt-get update > /dev/null 2>&1
+#     sudo su -c "gem install css_parser > /dev/null 2>&1" 
+#     sudo apt-get update > /dev/null 2>&1
 echo '##########################################################'
 echo '#  Laravel/Homstead user logon:                                         #'
 echo '##########################################################'
      echo 'greeter-show-manual-login=true' >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
+
+echo '##########################################################'
+echo '#   zsh                                                  #'
+echo '##########################################################'
+    #http://codurance.com/2015/03/16/installing-zprezto-a-quick-guide/
+    sudo apt-get -y install zsh
+    if [ ! -f /home/vagrant/.zshrc ]; then
+    touch /home/vagrant/.zshrc
+    fi
+    sudo rm  -R ~/.z*
+    zsh -c 'git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"'
+    zsh -c 'setopt EXTENDED_GLOB
+      for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+          ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done'
+    sudo chsh -s /bin/zsh
+    echo '##########################################################'
+    echo '#   Chrome                                               #'
+    echo '##########################################################'
+    cd /tmp
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo apt-get -f -y install
+    cd /home/vagrant
+    echo '##########################################################'
+    echo '##########################################################'
      echo 'Thats all folks!'
     diff=$(($date2-$date1))
     echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
